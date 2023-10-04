df46218c2e807abef3fc5a013a438516d3ba671d
Nana Yaa Adomaa Doku-Amponsah <dokuamponsahnanayaaadomaa@gmail.com>
2023-10-04 17:30:11 +0000

    django project

--- studentsweb/models.py
+++ studentsweb/models.py
@@ -21,6 +21,9 @@ class Room(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
 
+    class Meta:
+        ordering=['-updated', '-created'] #order in ascending order
+
     def __str__(self):
         return self.name
     
@@ -34,3 +37,16 @@ class Message(models.Model):
 
     def __str__(self):
         return self.body[0:50]
+
+# ----------------------------------------------------------------------------------------------------------------------------------
+class Student(models.Model):
+    student_number = models.PositiveIntegerField()
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=200)
+    field_of_study = models.CharField(max_length=50)
+    gpa = models.FloatField()
+    
+
+    def __str__(self):
+        return f'Student: {self.first_name} {self.last_name}'
--- studentsweb/views.py
+++ studentsweb/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
 # from django.http import HttpResponse
+from .forms import RoomForm
 
 
 # Create your views here.
+
 # def home(request):
 #     return HttpResponse('Home Page')
 
@@ -37,3 +39,30 @@ def room(request, pk):
     return render(request, 'studentsweb/room.html', context)
 
 
+def createRoom(request):
+    form = RoomForm()
+    # if request.method == 'POST':
+    #     print(request.POST)
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+
+    context = {'form' : form}
+
+    return render(request, 'studentsweb/room_form.html', context)
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context={'form' : form}
+    return render(request, 'studentsweb/room_form.html', context)
